84db035a10244e3a58451093a94b0980bcff221d
LyhourChhen <ukamilove66@gmail.com>
2021-04-08 23:51:42 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React, { Fragment } from "react";
-import { DatePicker } from "antd";
+import React, { Fragment, useState } from "react";
+import { Input, Typography } from "antd";
+import styled from "styled-components";
+import QRcode from "qrcode.react";
 
+const Flexbox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Spacer = styled.div`
+  padding: 10px;
+`;
 const App = () => {
+  const [value, setValue] = useState<string>("");
   return (
     <Fragment>
       <div
@@ -13,8 +25,20 @@ const App = () => {
         }}
       >
         <div>
-          <h1>Hello world</h1>
-          <DatePicker />
+          <Flexbox>
+            <Typography.Text strong>QRCode Generator</Typography.Text>
+          </Flexbox>
+          <Spacer />
+          <Input
+            placeholder={"Enter Value"}
+            style={{ width: 300 }}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <Spacer />
+          <Flexbox>
+            <QRcode value={value} autoSave={"true"} />
+          </Flexbox>
         </div>
       </div>
     </Fragment>
